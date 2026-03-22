f55430544f50488c6da36a0956294b752f4491bf
lethalfart <fuckclankers@tutamail.com>
2026-03-22 18:13:21 -0400

    Delete ChudWareSrc directory

--- ChudWareSrc/src/main/java/com/lethalfart/ChudWare/eventbus/impl/misc/PreMotionAlwaysEvent.java
+++
@@ -1,9 +0,0 @@
-package com.lethalfart.ChudWare.eventbus.impl.misc;
-
-import com.lethalfart.ChudWare.eventbus.impl.Event;
-
-public class PreMotionAlwaysEvent extends Event
-{
-    public static final PreMotionAlwaysEvent INSTANCE = new PreMotionAlwaysEvent();
-    private PreMotionAlwaysEvent() {}
-}
